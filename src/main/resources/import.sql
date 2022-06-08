75f5fb50653c0bcf074a43771e4b13bcad077177
samuel14luis <luismendoza.samuel@gmail.com>
2022-06-08 12:39:12 -0500

    import default data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO products (name, price, create_At) VALUES ('Sony', 700, NOW());
+INSERT INTO products (name, price, create_At) VALUES ('Panasonic', 800, NOW());
+INSERT INTO products (name, price, create_At) VALUES ('Apple', 1000, NOW());
+INSERT INTO products (name, price, create_At) VALUES ('Sony Notebook', 1000, NOW());
+INSERT INTO products (name, price, create_At) VALUES ('Hewlett Packard', 500, NOW());
+INSERT INTO products (name, price, create_At) VALUES ('Bianchi', 600, NOW());
+INSERT INTO products (name, price, create_At) VALUES ('Nike', 100, NOW());
+INSERT INTO products (name, price, create_At) VALUES ('Adidas', 200, NOW());
+INSERT INTO products (name, price, create_At) VALUES ('Reebok', 300, NOW());
